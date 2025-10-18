ab48844a67ba6f907489a1f65711c82e79e5fe6f
Siddhant <siddhantvgorte@gmail.com>
2025-10-18 23:53:55 +0530

    restarted learning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,12 @@
-import Chai from './chai'
+
+
+
+
 
 function App() {
-  const username = 'chai aur code'
 
   return (
-    <>
-    <Chai/>
-    <h1>chai aur react {username}</h1>
-    <p>test para</p>
-    </>
+    <h1>chai aur react with vite</h1>
   )
 }
 
